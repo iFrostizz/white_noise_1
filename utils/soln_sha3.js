e00e8b556389f1363609ccd30b483f15fdf763b4
0xdineshkumarsm <105421120+0xdineshkumarsm@users.noreply.github.com>
2022-09-22 09:52:39 +0530

    script correction

--- utils/soln_sha3.js
+++ utils/soln_sha3.js
@@ -9,7 +9,7 @@ let max = 2 ** 64
 while(salt < max){
     hash = utils.keccak256(utils.solidityPack(["uint32","address","uint64"],[prefix,address,salt]))
 
-    if (hash.substring(62,66).toLowerCase() == "d073" && parseInt(hash.substring(2,6),16) == 875){
+    if (parseInt(hash.substring(62,66),16) == 53363 && parseInt(hash.substring(2,6),16) == 875){
         console.log("salt,hash",salt,hash,utils.solidityPack(["uint32","uint64"],[prefix,salt]))
         break
     }
