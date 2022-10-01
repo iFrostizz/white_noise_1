4b63e8a3553b6db04d5ac203902c556c500eb629
francois <fgg67@hotmail.fr>
2022-10-02 00:15:52 +0200

    bootstrap

--- test/Challenge.t.sol
+++ test/Challenge.t.sol
@@ -1,19 +1,29 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.13;
+pragma solidity ^0.8.0;
 
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
+interface IChallenge {
+    
+}
+
+interface ISolve {
+    
+}
+
 contract ChallengeTest is Test {
-    address challenge;
+    IChallenge challenge;
+    ISolve solve;
 
     function setUp() public {
-        challenge = HuffDeployer.deploy("Challenge");
-        console.log(challenge);
-        console.logBytes(address(challenge).code);
+        challenge = IChallenge(HuffDeployer.deploy("Challenge"));
+        console.log("challenge deployed at %s !", address(challenge));
     }
 
-    function testSetAndGetValue(uint256 value) public {
+    function testSolve() public {
+        solve = ISolve(HuffDeployer.deploy("Solution"));
+        console.log("solution deployed at %s !", address(solve));
    }
 }
